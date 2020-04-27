cee45d0e761a304e5f1ba8a01dbe32a13c19d188
Lakshmanan <uilaxman@gmail.com>
2020-04-28 01:45:17 +0530

    Cors added

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ const express = require("express");
 const app = express();
 const bodyparser = require("body-parser");
 const port = process.env.PORT || 3200;
+const cors = require('cors');
 
 const BlogController = require('./Controller/blogController');
 require('./Config/db');
 
+app.use(cors({credentials: true, origin: true }));
+app.options('*', cors());
+
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 
+app.get('/', (req, res) => res.send('Working!!!'));
 app.use('/blogs', BlogController);
 
 
