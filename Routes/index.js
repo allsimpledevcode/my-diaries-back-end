5ee166898f278e624f65130152bc9c173131b7e4
Lakshmanan <uilaxman@gmail.com>
2020-09-24 21:49:20 +0530

    Removed unused model and controller

--- Routes/index.js
+++ Routes/index.js
@@ -2,13 +2,9 @@ const express = require("express");
 const routers = express.Router();
 const blogRoutes = require('./blogRoute');
 const diaryRoutes = require('./diaryRoute');
-const tagRoutes = require('./tagRoute');
-const userRoutes = require('./userRoute');
 
 routers.use("/api/blogs", blogRoutes);
 routers.use("/api/diaries", diaryRoutes);
-routers.use("/api/tags", tagRoutes);
 
-routers.use("/user", userRoutes);
 
 module.exports = routers;
